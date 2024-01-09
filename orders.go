df700975b42298d448b7ecf54320e4cb1c77c4f3
PritibardhanJha2 <2019pgcaca44@nitjsr.ac.in>
2024-01-09 22:33:35 +0530

    added trailing sl

--- orders.go
+++ orders.go
@@ -46,18 +46,19 @@ type Orders []Order
 
 // OrderParams represents parameters for placing an order.
 type OrderParams struct {
-	Variety         string `json:"variety"`
-	TradingSymbol   string `json:"tradingsymbol"`
-	SymbolToken     string `json:"symboltoken"`
-	TransactionType string `json:"transactiontype"`
-	Exchange        string `json:"exchange"`
-	OrderType       string `json:"ordertype"`
-	ProductType     string `json:"producttype"`
-	Duration        string `json:"duration"`
-	Price           string `json:"price"`
-	SquareOff       string `json:"squareoff"`
-	StopLoss        string `json:"stoploss"`
-	Quantity        string `json:"quantity"`
+	Variety          string `json:"variety"`
+	TradingSymbol    string `json:"tradingsymbol"`
+	SymbolToken      string `json:"symboltoken"`
+	TransactionType  string `json:"transactiontype"`
+	Exchange         string `json:"exchange"`
+	OrderType        string `json:"ordertype"`
+	ProductType      string `json:"producttype"`
+	Duration         string `json:"duration"`
+	Price            string `json:"price"`
+	SquareOff        string `json:"squareoff"`
+	StopLoss         string `json:"stoploss"`
+	Quantity         string `json:"quantity"`
+	TrailingStopLoss string `json:"trailingstoploss"`
 }
 
 // OrderParams represents parameters for modifying an order.
